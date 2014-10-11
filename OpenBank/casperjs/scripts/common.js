fb1c5094ee446e0d1cdf18566763f4e2c9bdf2e4
Brady Holt <brady.holt@gmail.com>
2014-10-11 15:57:04 -0500

    Setting phantomJs user agent to be Chrome

--- OpenBank/casperjs/scripts/common.js
+++ OpenBank/casperjs/scripts/common.js
@@ -9,7 +9,8 @@ var casper = require('casper').create({
 	verbose: true, 
 	logLevel: 'debug',
 	pageSettings: { 
-        webSecurityEnabled: false 
+        webSecurityEnabled: false,
+        userAgent: 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.2 Safari/537.36' 
     }
 });
 
